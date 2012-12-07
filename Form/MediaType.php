573132fc6ed1066aee7bf0810d8fa38d9b6b2046
Krzysztof Ożóg <zokowski@gmail.com>
2012-12-08 00:30:51 +0100

    Form Type as a service

--- Form/MediaType.php
+++ Form/MediaType.php
@@ -3,19 +3,42 @@
 namespace Coshi\MediaBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class MediaType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+
+    protected $class;
+
+    public function __construct($className)
+    {
+        $this->class = $className;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file','file');
+            ->add('file', 'file');
         ;
     }
+
+    public function setDefaultOptions(OptionsResolverInterace $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => $this->class
+            )
+        );
+    }
+
+
+
 
     public function getName()
     {
         return 'coshi_mediabundle_mediatype';
     }
+
+
 }
